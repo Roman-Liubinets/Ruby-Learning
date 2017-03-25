c2c52c9efc6c0ef8a4a2a2bebf1f2f9ae46bff82
Roman.Liubinets <roman.liubinets@gmail.com>
2017-03-25 19:20:22 +0200

    init student

---
+++ student2.rb
@@ -0,0 +1,50 @@
+class Student
+  attr_reader :first_name, :last_name, :grade
+  def initialize(first_name, last_name, grade)
+    @first_name = first_name
+    @last_name = last_name
+    @grade = grade
+  end
+
+  def senior?
+    @grade ==12
+  end
+
+  def to_s
+    "#{first_name}, #{last_name}"
+  end
+end
+
+
+=begin
+def seniors(students)
+  senior_students =[]
+  students.each do |student|
+  if student.senior?
+    senior_students.push(student)
+  end
+end
+return senior_students
+end
+=end
+
+def seniors(students)
+  students.select {|student| student.senior?}
+end
+
+
+
+
+fred = Student.new("Fred", "James", 12)
+sarah = Student.new("Sarah", "Smith", 12)
+jack = Student.new("Jack", "Gong", 10)
+
+all_students = [fred, sarah, jack]
+
+#all_students.each do |student|
+#  puts student
+#end
+
+seniors(all_students).each do |student|
+  puts student
+end
